7f9e71b4ff7b41c4a8a2afd18d4fb3e1fedfefc1
K.hemanth <hemanthkothyala1453@gmail.com>
2025-04-06 17:30:56 +0530

    pushed last

--- server.js
+++ server.js
@@ -273,12 +273,6 @@ app.post("/logout", (req, res) => {
   req.session.destroy(() => res.redirect("/login"));
 });
 
-app.get("/", (req, res) => {
-  res.redirect("/login");
-});
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
 });
