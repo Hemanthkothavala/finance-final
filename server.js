c7e65a058b2e61ce28651262419bad9aadbd2bf0
K.hemanth <hemanthkothyala1453@gmail.com>
2025-04-06 17:36:10 +0530

    changed images and home page

--- server.js
+++ server.js
@@ -273,6 +273,12 @@ app.post("/logout", (req, res) => {
   req.session.destroy(() => res.redirect("/login"));
 });
 
-app.listen(4000, () => {
-  console.log("Server is running on port 4000");
+app.get("/", (req, res) => {
+  res.redirect("/login");
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
